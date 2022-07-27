cfc4f4f73f289b9af143026964c206842cbe9207
kumarn0151 <99696234+kumarn0151@users.noreply.github.com>
2022-07-27 12:27:12 +1200

    Add files via upload

--- 01_Base component.py
+++ 01_Base component.py
@@ -1,5 +1,4 @@
-import random 
-
+import random
 # Ask user if they have played before
 def yes_no(question):
     valid = False
@@ -8,13 +7,23 @@ def yes_no(question):
 
         response = input(question).lower()
 
-        if response == "yes" or response == "y":
+        if response == "yes" or response == "y": 
             return "yes"
         elif response == "no" or response == "n":
             return "no"
         else:
             print("please answer yes/no")
 
+
+    response = input (question).lower()
+def choice_checker(question, error): 
+
+
+    valid = False
+    while not valid:
+
+        # Ask user for choice (and put choice in lowercase) 
+        response = input (question).lower()
 
 def instructions():
     print()
@@ -32,7 +41,7 @@ def instructions():
     print()
 
 # Show instructions
-print("*** Welcome to Math quiz game *** ")
+print("*** Welcome to Math quiz *** ")
 print()
 played_before = yes_no("Have you played the game before?")
 
@@ -42,8 +51,8 @@ if played_before == "no":
 
 
 def check_rounds():
-    while True:
-        response = input("How many questions would you like: ")
+    while True: 
+        response = input ("How many questions would you like: ")
 
         round_error = "please type either <enter> or an integer that is more than 0"
 
@@ -53,49 +62,70 @@ def check_rounds():
 
                 if response < 1:
                     print(round_error)
-                    continue
+                    continue 
 
             except ValueError:
                 print(round_error)
-                continue
+                continue 
 
         return response
 
 
 # Main routine goes here...
 
-rounds_played = 0
-choose_instruction = "please choose a shape between (Square, Triangle and rectangle)"
+rounds_played = 0 
+choose_instruction = "please press enter to begin"
 
 # Ask user for # of rounds, <enter> for infinite mode
-rounds = check_rounds()
+rounds = check_rounds ()
 
-end_game = "no"
+end_game="no"
 while end_game == "no":
 
-    # Rounds Heading
+    # Rounds Heading 
     print()
-    if rounds == "":
-        heading = "continues Mode: Round {}".format(rounds_played + 1)
-        print(heading)
-        choose = input("{} or 'xxx' to end: ".format(choose_instruction))
-        if choose == "xxx":
-            break
-
+    if rounds =="":
+       heading = "continues Mode: Round {}".format(rounds_played + 1)
+       print (heading) 
+       choose = input ("{} or 'xxx' to end: ".format(choose_instruction))
+       if choose == "xxx":
+           break 
+      
     else:
-        heading = "Round {} of {}".format(rounds_played + 1, rounds)
-        print(heading)
+        heading = "question {} of {}".format(rounds_played + 1, rounds)
+        print (heading)
         choose = input(choose_instruction)
         if rounds_played == rounds - 1:
-            end_game = "yes"
+           end_game = "yes"
 
-    # rest of loop / game
-    print("you chose {}".format(choose))
+    # rest of loop / game 
 
-    rounds_played += 1
+    rounds_played += 1 
 
+# Genarate questions 
 
+    question1 = input("A rectangle with a side of 4cm and a side of 3cm has an area of ___ cm squared")
+    if question1 == "12":
+        print("CORRECT ")
+    else: 
+        question1 = "{}".format
+        print("INCORRECT!  the correct answer is 12")
 
+question2 = input("A triangle has a base of 3 and a height of 6.  The area is ___ cm squared.")
+if question2 == "9":
+    print("CORRECT!")
+else:
+    question2 = "{}".format
+    print("INCORRECT! the correct answer is 9")
+
+    question3 = input("A Square has sides of 5. The area is ___ cm squared")
+    if question3 == "25":
+        print("CORRECT!")
+    else:
+        question3 = "{}".format
+        print("INCORRECT! the correct answer is 25")
+        
+                    
 
 
 
